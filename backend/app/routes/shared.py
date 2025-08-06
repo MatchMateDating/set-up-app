0a80d51a949adb76b5e2484d6b92554d441ccef0
allyson-y <54675313+allyson-y@users.noreply.github.com>
2025-08-05 22:09:39 -0400

    Fix shared file

--- backend/app/routes/shared.py
+++ backend/app/routes/shared.py
@@ -1,6 +1,8 @@
+from flask import jsonify
 from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from functools import wraps
+from app.models.userDB import User
 
 def token_required(f):
     @wraps(f)
