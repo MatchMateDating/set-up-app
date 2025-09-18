b1e3a43522d8b8d812c7d8e7692d10c1f6614851
allyson-y <54675313+allyson-y@users.noreply.github.com>
2025-09-17 22:48:51 -0400

    Add comments for which line to run for which type of DB instance

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,15 +1,18 @@
 import os
 
 class Config:
-    DB_USERNAME = os.getenv('DB_USERNAME', 'your_username')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'your_password')
-    DB_HOST = os.getenv('DB_HOST', 'your-db-endpoint')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_NAME = os.getenv('DB_NAME', 'postgres')
+    # For running the DB on Amazon RDS for PostgreSQL
+    # DB_USERNAME = os.getenv('DB_USERNAME', 'your_username')
+    # DB_PASSWORD = os.getenv('DB_PASSWORD', 'your_password')
+    # DB_HOST = os.getenv('DB_HOST', 'your-db-endpoint')
+    # DB_PORT = os.getenv('DB_PORT', '5432')
+    # DB_NAME = os.getenv('DB_NAME', 'postgres')
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(SQLALCHEMY_DATABASE_URI)
+    # SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
+    # For running the DB locally
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/users.db'
+
+    # For either
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret-key')
