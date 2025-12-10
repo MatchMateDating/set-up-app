ec678552793996dbc4958993f4c9d167008d6d86
Cruedy <cruedy@hmc.edu>
2025-12-09 18:07:24 -0800

    retest

--- matchmaker-mobile/api.js
+++ matchmaker-mobile/api.js
@@ -1,8 +1,6 @@
 import { API_BASE_URL } from '@env';
 import { Platform } from "react-native";
 
-export const BASE_URL =
-  Platform.OS === "android"
-    ? API_BASE_URL.replace("localhost", "10.0.2.2").replace("http://192.168.0.116:5000", "10.0.2.2")
-    : API_BASE_URL;
+export const BASE_URL = API_BASE_URL;
+
 
